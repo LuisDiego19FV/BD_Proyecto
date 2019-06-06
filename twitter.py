bd8680e95b73cde19f7eea7f3e7dc98fa5a43d8b
sergiomarchena16 <mar16387@uvg.edu.gt>
2019-06-05 19:56:51 -0600

    hello world in twitter - python
    tweet "hello world" in twitter (requieres an additional python script with keys and tokens)

---
+++ twitter.py
@@ -0,0 +1,13 @@
+from twython import Twython
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+twitter = Twython (consumer_key, consumer_secret, access_token, access_token_secret)
+message = "Hello world! @TwitterAPI"
+twitter.update_status(status=message)
+print("Tweeted: %s" % message)
